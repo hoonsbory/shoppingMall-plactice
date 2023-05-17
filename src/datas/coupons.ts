b092680afb20eb047ac253f052fdb3bb9df4161f
hoonsbory <tonymkcv@naver.com>
2023-05-17 20:18:31 +0900

    [init] API DATA Object & interface 생성

---
+++ src/datas/coupons.ts
@@ -0,0 +1,19 @@
+export interface ICoupon {
+  type: string;
+  title: string;
+  discountRate?: number;
+  discountAmount?: number;
+}
+
+export const coupons: ICoupon[] = [
+  {
+    type: 'rate',
+    title: '10% 할인 쿠폰',
+    discountRate: 10,
+  },
+  {
+    type: 'amount',
+    title: '10,000원 할인 쿠폰',
+    discountAmount: 10000,
+  },
+];
