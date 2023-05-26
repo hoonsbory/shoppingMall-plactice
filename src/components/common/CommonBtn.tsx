12de50b56f14c5ef9cd13294bbb2c59531d69620
hoonsbory <tonymkcv@naver.com>
2023-05-27 04:14:31 +0900

    [feat] 공용 버튼 컴포넌트 개발

---
+++ src/components/common/CommonBtn.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+
+const CommonBtn = ({ children, onClick }: { children: string; onClick: () => void }) => {
+  return (
+    <button
+      onClick={onClick}
+      css={css`
+        padding: 10px 15px;
+        background: black;
+        color: white;
+        border-radius: 5px;
+      `}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CommonBtn;
