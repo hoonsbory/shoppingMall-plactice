1172b1d3977c07f2ba9f7ac5f1749b5f8231390f
hoonsbory <tonymkcv@naver.com>
2023-05-27 04:10:18 +0900

    [feat] 장바구니 총 금액 정보 컴포넌트 개발 총 주문 금액, 할인 금액, 결제금액을 받아서 렌더링하는 뷰

---
+++ src/components/cart/CartResult.tsx
@@ -0,0 +1,43 @@
+import { commonTableCellCss, commonTableCss } from '@/styles/cart';
+import { css } from '@emotion/react';
+interface ICartResult {
+  totalOrderPrice: number;
+  discountedPrice: number;
+  totalPaymentPrice: number;
+}
+const CartResult = ({ totalOrderPrice, discountedPrice, totalPaymentPrice }: ICartResult) => {
+  return (
+    <section css={sectionCss}>
+      <div css={tableHeader}>
+        <div css={tableRow('36%')}>총 주문금액</div>
+        <div css={tableRow('28%')}>총 할인금액</div>
+        <div css={tableRow('36%')}>총 결제금액</div>
+      </div>
+      <div css={commonTableCss}>
+        <div css={tableRow('36%')}>{totalOrderPrice.toLocaleString()}원</div>
+        <div css={tableRow('28%')}>{discountedPrice.toLocaleString()}원</div>
+        <div css={tableRow('36%')}>{totalPaymentPrice.toLocaleString()}원</div>
+      </div>
+    </section>
+  );
+};
+
+export default CartResult;
+
+const sectionCss = css`
+  margin: 117px auto 137px;
+  font-size: 12px;
+  border-top: 1px solid rgb(0, 0, 0);
+`;
+
+const tableHeader = css`
+  ${commonTableCss}
+  border-bottom: 1px solid rgb(228, 228, 228);
+`;
+
+const tableRow = (width: string) => css`
+  ${commonTableCellCss}
+  height: 74px;
+  text-align: center;
+  width: ${width};
+`;
