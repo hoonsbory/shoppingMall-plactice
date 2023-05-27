70c3fddca459fe0c3071eee3604c7cbeb3548d57
hoonsbory <tonymkcv@naver.com>
2023-05-27 21:52:10 +0900

    [refactor] cart로부터 컴포넌트 분리 및 useQuery를 이용한 promise상태에 따른 렌더링 추가

--- src/components/cart/SelectCoupon.tsx
+++ src/components/cart/SelectCoupon.tsx
@@ -1,21 +1,27 @@
 import { ICoupon } from '@/interfaces/couponInterface';
+import { css } from '@emotion/react';
+import axios from 'axios';
 import { ChangeEvent } from 'react';
+import { useQuery } from 'react-query';
+
+const getCoupons = async (): Promise<ICoupon[]> => {
+  const res = await axios.get('/api/getCoupons');
+  return res.data;
+};
 
 const SelectCoupon = ({
   handleSelectChange,
-  coupons,
 }: {
-  handleSelectChange: (e: ChangeEvent<HTMLSelectElement>) => void;
-  coupons: ICoupon[];
+  handleSelectChange: (e: ChangeEvent<HTMLSelectElement>, couponse: ICoupon[]) => void;
 }) => {
+  const { data: coupons } = useQuery(['getCoupons'], getCoupons);
+
   return (
-    <select onChange={handleSelectChange}>
+    <select onChange={e => handleSelectChange(e, coupons!)}>
       <option value="" hidden>
         쿠폰을 선택해주세요
       </option>
-      {coupons.map((coupon, idx) => (
-        <option value={idx}>{coupon.title}</option>
-      ))}
+      {coupons && coupons.map((coupon, idx) => <option value={idx}>{coupon.title}</option>)}
     </select>
   );
 };
