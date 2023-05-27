62fa41c6a51ec24dbaf7666b7f1f6a677af32847
hoonsbory <tonymkcv@naver.com>
2023-05-28 02:39:19 +0900

    [chore] 미사용 모듈  import 제거 => Hydrate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
