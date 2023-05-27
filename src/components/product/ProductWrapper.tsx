9d6303f8ea9b3f6c96adb5c2663e474b874ef701
hoonsbory <tonymkcv@naver.com>
2023-05-27 12:53:43 +0900

    [refactor] 카트 핸들링 함수를 자식 product에서 생성 score기준 내림차순 정렬 display grid로 반응형 적용 각 상품의 장바구니 담김 여부 판별

--- src/components/product/ProductWrapper.tsx
+++ src/components/product/ProductWrapper.tsx
@@ -1,13 +1,46 @@
-import { IProducts } from '@/interfaces/productInterface';
+import Modal from '@/components/common/Modal';
+import Portal from '@/components/common/Protal';
+import ProductBox from '@/components/product/ProductBox';
+import { IProducts, IProduct } from '@/interfaces/productInterface';
+import useCartStore from '@/store/cartStore';
+import useModalStore from '@/store/modalStore';
+import { css } from '@emotion/react';
 import { useQuery } from 'react-query';
-
 interface IProductWrapper {
   queryFn: () => Promise<IProducts>;
   queryKey: (string | number)[];
 }
 
 const ProductWrapper = ({ queryFn, queryKey }: IProductWrapper) => {
+  const cartedProducts = useCartStore(state => state.products);
   const { data } = useQuery(queryKey, queryFn);
-  return <div>{data && data.products.map(product => <p>{product.item_name}</p>)}</div>;
+
+  data?.products.sort((a, b) => b.score - a.score);
+  return (
+    <>
+      <Portal selector="#portal">
+        <Modal />
+      </Portal>
+      <ul css={wrapperCss}>
+        {data &&
+          data.products.map(product => (
+            <ProductBox
+              product={product}
+              // 카트에 담겨진 상품 판별
+              isCarted={cartedProducts.some(i => i.item_no === product.item_no)}
+            />
+          ))}
+      </ul>
+    </>
+  );
 };
 export default ProductWrapper;
+
+export const wrapperCss = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, max-content));
+  column-gap: 15px;
+  row-gap: 70px;
+  justify-content: center;
+  padding: initial;
+`;
