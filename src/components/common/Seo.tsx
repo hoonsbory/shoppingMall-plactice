94d771cacb3baa65a0f39cb2e5f9fb66e4f77ca1
hoonsbory <tonymkcv@naver.com>
2023-05-28 02:37:20 +0900

    [chore] SEO 태그 컴포넌트 개발 및 document 적용

---
+++ src/components/common/Seo.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+
+const Seo = () => {
+  return (
+    <>
+      <title>감도 깊은 취향 셀렉트샵 29CM</title>
+      <meta property="og:site_name" content="감도 깊은 취향 셀렉트샵 29CM" />
+      <meta property="og:title" content="감도 깊은 취향 셀렉트샵 29CM" />
+      <meta property="og:url" content="https://www.29cm.co.kr/" />
+      <meta property="og:image" content="https://img.29cm.co.kr/mall/og_image.png" />
+      <meta
+        name="og:description"
+        content="패션, 라이프스타일, 컬처까지 29CM만의 감도 깊은 셀렉션을 만나보세요"
+      />
+      <meta
+        name="twitter:description"
+        content="패션, 라이프스타일, 컬처까지 29CM만의 감도 깊은 셀렉션을 만나보세요"
+      />
+      <meta name="twitter:title" content="감도 깊은 취향 셀렉트샵 29CM" />
+      <meta name="twitter:url" content="https://www.29cm.co.kr/" />
+      <meta name="twitter:image" content="https://img.29cm.co.kr/mall/og_image.png" />
+      <meta property="og:type" content="company" />
+      <meta
+        name="description"
+        content="패션, 라이프스타일, 컬처까지 29CM만의 감도 깊은 셀렉션을 만나보세요"
+      />
+      <link rel="icon" type="image/x-icon" href="favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+};
+
+export default Seo;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Seo from '@/components/common/Seo';
+import { Html, Main, NextScript, Head } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <Seo />
+      </Head>
       <body>
         <div id="portal" />
         <Main />
