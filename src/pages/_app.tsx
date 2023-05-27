28675c38c3673a4172ec83b236bd4cf88d51b63b
hoonsbory <tonymkcv@naver.com>
2023-05-27 22:04:35 +0900

    [chore&fix] queryClientProvider 추가 및 설정 기본적으로 suspense 사용 refetch 미사용
    ====fixed====
    nextjs는 URL이 변경될때마다 App컴포넌트를 렌더링하고 이 때 queryClient가
    계속 재생성되면서 무한 request 발생
    useState로 재생성을 막아서 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            suspense: true,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
